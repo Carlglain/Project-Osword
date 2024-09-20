8b95e199566cfe8c0ec9b06d070b318ba21c6d41
Carlstorm <carlglainnyuysemo@gmail.com>
2024-09-20 23:07:58 +0100

    update front end

--- frontend/src/pages/loginPage/Registration.jsx
+++ frontend/src/pages/loginPage/Registration.jsx
@@ -18,7 +18,7 @@ function Login() {
     <div className='fcontainer'>
       <h1>Welcome to Lorem i</h1>
       <RLButton onChooseRegPage={handleOnChooseRegPage}/>
-     <p>Lorem ipsum dolor sit amet,adipiscing elit. Sed gfdut labore et dolore magna aliqua.</p>
+     <p>Lorem ipsum dolor sit amet,adka dka dipiscing elit. Sed gfdut labore et dolore magna aliqua.</p>
       <form >
         {/* email rendered only when we want to create account */}
         <label htmlFor="l0">Email Address</label> <br />
